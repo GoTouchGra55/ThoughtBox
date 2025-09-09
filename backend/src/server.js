851018974e4abd8dcef4cd5fc4c8d9133d05b473
GoTouchGra55 <shauryatamang2009@gmail.com>
2025-09-09 18:12:49 +0545

    Prepared for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,27 +1,44 @@
 require("dotenv").config();
 const express = require("express");
-const notesRouter = require("./routes/Routes");
+const cors = require("cors");
+const path = require("path");
+
+const Routes = require("./routes/Routes");
 const connectDB = require("./config/db");
 const rateLimiter = require("./middleware/rateLimiter");
-const cors = require("cors");
-
-const PORT = process.env.PORT || 5000;
 
 const app = express();
+const PORT = process.env.PORT || 5001;
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-  })
-);
-app.use(express.json());
+// middleware
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5001",
+    })
+  );
+}
+app.use(express.json()); // this middleware will parse JSON bodies: req.body
 app.use(rateLimiter);
 
-app.use("/api/notes", notesRouter);
+// our simple custom middleware
+// app.use((req, res, next) => {
+//   console.log(`Req method is ${req.method} & Req URL is ${req.url}`);
+//   next();
+// });
+
+app.use("/api/notes", Routes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+
+  app.get(/^(?!\/api).*/, (req, res) => {
+    res.sendFile(path.join(__dirname, "../../frontend", "dist", "index.html"));
+  });
+}
 
 connectDB().then(() => {
-  app.listen(PORT, (err) => {
-    if (err) throw new Error(err);
-    console.log(`Server running on port: ${PORT}`);
+  app.listen(PORT, () => {
+    console.log("Server started on PORT:", PORT);
   });
 });
